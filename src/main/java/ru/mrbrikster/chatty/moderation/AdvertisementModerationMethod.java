c3147cf967cd43cfc9543db49cf33e104e1b5367
JekaRUS <jekarus47@gmail.com>
2018-10-26 00:03:51 +0300

    new features for advertising moderating

--- src/main/java/ru/mrbrikster/chatty/moderation/AdvertisementModerationMethod.java
+++ src/main/java/ru/mrbrikster/chatty/moderation/AdvertisementModerationMethod.java
@@ -1,19 +1,28 @@
 package ru.mrbrikster.chatty.moderation;
 
+import lombok.Getter;
 import ru.mrbrikster.chatty.config.ConfigurationNode;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class AdvertisementModerationMethod extends ModerationMethod {
 
+    private String editedMessage;
+    private boolean alreadyChecked = false, previousResult = false;
+
     private final List<String> whitelist;
     private Pattern ipPattern, webPattern;
+    private String adPlaceholder;
+    @Getter
+    private boolean useBlock;
 
     AdvertisementModerationMethod(ConfigurationNode configurationNode, String message) {
         super(message);
+        this.editedMessage = null;
 
         this.whitelist = configurationNode.getNode("whitelist")
                 .getAsStringList().stream().map(String::toLowerCase).collect(Collectors.toList());
@@ -21,47 +30,62 @@ public class AdvertisementModerationMethod extends ModerationMethod {
                 .getAsString("(?:\\d{1,3}[.,\\-:;\\/()=?}+ ]{1,4}){3}\\d{1,3}"));
         this.webPattern = Pattern.compile(configurationNode.getNode("patterns.web")
                 .getAsString("[-a-zA-Z0-9@:%_\\+~#?&//=]{2,256}\\.[a-z]{2,4}\\b(\\/[-a-zA-Z0-9@:%_\\+~#?&//=]*)?"));
+        this.adPlaceholder = configurationNode.getNode("ad-placeholder").getAsString("{ad}");
+        this.useBlock = configurationNode.getNode("block").getAsBoolean(true);
     }
 
     @Override
     public String getEditedMessage() {
-        return message;
+        if (this.editedMessage == null) {
+            this.isBlocked();
+        }
+        return this.editedMessage;
     }
 
     @Override
     public boolean isBlocked() {
-        return containsIP(message) || containsURL(message);
+        if (this.alreadyChecked) {
+            return this.previousResult;
+        }
+        this.previousResult = match(ipPattern, str -> str);
+        this.previousResult = this.previousResult || match(webPattern, str -> str
+                .replaceAll("www.", "")
+                .replaceAll("http://", "")
+                .replaceAll("https://", ""));
+
+        this.alreadyChecked = true;
+        return this.previousResult;
     }
 
-    private boolean containsIP(String message) {
-        Matcher regexMatcher = ipPattern.matcher(message);
+    private boolean match(Pattern pattern, Function<String, String> modifyString) {
+        Matcher matcher = pattern.matcher(this.message);
 
-        while (regexMatcher.find()) {
-            String advertisement = regexMatcher.group().trim().toLowerCase();
+        int prevIndex = 0;
+        StringBuilder builder = new StringBuilder();
 
-            if (!whitelist.contains(advertisement)) {
-                return true;
+        boolean hasAds = false;
+        while (matcher.find()) {
+            String group = matcher.group();
+
+            builder.append(this.message, prevIndex, matcher.start());
+            prevIndex = matcher.end();
+
+            String ad = group.trim().toLowerCase();
+            ad = modifyString.apply(ad);
+
+            if (this.whitelist.contains(ad)) {
+                builder.append(this.message, matcher.start(), matcher.end());
+            } else {
+                hasAds = true;
+                builder.append(this.adPlaceholder);
             }
         }
 
-        return false;
-    }
-
-    private boolean containsURL(String message) {
-        Matcher regexMatcher = webPattern.matcher(message);
-
-        while (regexMatcher.find()) {
-            String advertisement = regexMatcher.group().trim().toLowerCase()
-                    .replaceAll("www.", "")
-                    .replaceAll("http://", "")
-                    .replaceAll("https://", "");
-
-            if (!whitelist.contains(advertisement)) {
-                return true;
-            }
+        if (prevIndex < this.message.length()) {
+            builder.append(this.message, prevIndex, this.message.length());
         }
-
-        return false;
+        this.editedMessage = builder.toString();
+        return hasAds;
     }
 
 }
