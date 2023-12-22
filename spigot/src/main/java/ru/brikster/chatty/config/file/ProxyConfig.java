694d7594fcd78bb643a05e6ac9a0d6799eb8a8b8
Ilya Andreev <mrbrikster@yandex.ru>
2023-12-22 18:30:08 +0300

    Update config comments

--- spigot/src/main/java/ru/brikster/chatty/config/file/ProxyConfig.java
+++ spigot/src/main/java/ru/brikster/chatty/config/file/ProxyConfig.java
@@ -21,7 +21,11 @@ public class ProxyConfig extends OkaeriConfig {
     @Comment({"", "Enable support for cross-server messaging?"})
     @Comment("This feature supports any proxy (BungeeCord, Velocity), ")
     @Comment("including multi-proxy networks.")
-    @Comment("You need to setup Redis and shared database to use it")
+    @Comment("You need to setup Redis and shared database to use it.")
+    @Comment
+    @Comment("Note: relational placeholders will not be parsed for cross-server messages.")
+    @Comment("Some placeholders of message target (in PM) also may not work (if plugin doesn't support")
+    @Comment("placeholders for offline players)")
     private boolean enable = false;
 
     @Comment({"", "Basic Redis configuration."})
@@ -29,7 +33,7 @@ public class ProxyConfig extends OkaeriConfig {
 
     @Comment({"", "Enable external Redis configuration for advanced setup."})
     @Comment("File with name \"redis_config.json\" will be created.")
-    @Comment("See \"Redisson\" library documentation for details.")
+    @Comment("See \"Redisson\" library documentation for details")
     private boolean useExternalRedisConfig = false;
 
     @Getter
@@ -44,9 +48,10 @@ public class ProxyConfig extends OkaeriConfig {
 
     }
 
-    @Comment({"", "Shared database configuration."})
+    @Comment({"", "Shared database configuration"})
+    @Comment("(database should be the same for every server).")
     @Comment("Possible types: POSTGRESQL, MYSQL.")
-    @Comment("Default port for PostgreSQL: 5432, for MySQL: 3306.")
+    @Comment("Default port for PostgreSQL: 5432, for MySQL: 3306")
     private DatabaseConfig databaseConfig = new DatabaseConfig();
 
     @Getter
